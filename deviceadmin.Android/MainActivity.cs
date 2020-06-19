7e2fcd310e433df9d9f099f918bd7e94fbb8f340
imanz <zolfaghari19@gmail.com>
2020-06-19 12:28:28 +0430

    Device Admin necessary things for android.

--- deviceadmin.Android/MainActivity.cs
+++ deviceadmin.Android/MainActivity.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 
 using Android.App;
+using Android.App.Admin;
+using Android.Content;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using sample_device_admin.Android.Receivers;
+using Xamarin.Forms;
 
 namespace deviceadmin.Droid
 {
     [Activity(Label = "deviceadmin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public const int RequestCodeEnableAdmin = 15;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -22,7 +27,58 @@ namespace deviceadmin.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            SetKioskMode(true);
         }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            if (requestCode == RequestCodeEnableAdmin)
+            {
+                PostSetKioskMode(resultCode == Result.Ok);
+            }
+            else
+                base.OnActivityResult(requestCode, resultCode, data);
+        }
+
+        public bool SetKioskMode(bool enable)
+        {
+            var deviceAdmin =
+                new ComponentName(Forms.Context.ApplicationContext, Java.Lang.Class.FromType(typeof(AdminReceiver)));
+            if (enable)
+            {
+                var intent = new Intent(DevicePolicyManager.ActionAddDeviceAdmin);
+                intent.PutExtra(DevicePolicyManager.ExtraDeviceAdmin, deviceAdmin);
+                // intent.PutExtra(DevicePolicyManager.ExtraAddExplanation, "activity.getString(R.string.add_admin_extra_app_text");
+                ((Activity) Forms.Context).StartActivityForResult(intent, MainActivity.RequestCodeEnableAdmin);
+                return false;
+            }
+            else
+            {
+                var devicePolicyManager =
+                    (DevicePolicyManager) Forms.Context.GetSystemService(Context.DevicePolicyService);
+                devicePolicyManager.RemoveActiveAdmin(deviceAdmin);
+                return true;
+            }
+        }
+		
+        private void PostSetKioskMode(bool enable)
+        {
+            if (enable)
+            {
+                var deviceAdmin = new ComponentName(Forms.Context.ApplicationContext,
+                    Java.Lang.Class.FromType(typeof(AdminReceiver)));
+                var devicePolicyManager =
+                    (DevicePolicyManager) Forms.Context.GetSystemService(Context.DevicePolicyService);
+                if (!devicePolicyManager.IsAdminActive(deviceAdmin)) throw new Exception("Not Admin");
+
+                ((Activity) Forms.Context).StartLockTask();
+            }
+            else
+            {
+                ((Activity) Forms.Context).StopLockTask();
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
---
+++ deviceadmin.Android/Receivers/AdminReceiver.cs
@@ -0,0 +1,14 @@
+﻿﻿using Android.App;
+using Android.App.Admin;
+using Android.Content;
+
+namespace sample_device_admin.Android.Receivers
+{
+	[BroadcastReceiver(Permission = "android.permission.BIND_DEVICE_ADMIN",
+		Name = "sample_device_admin.Android.Receivers.AdminReceiver")]
+	[MetaData("android.app.device_admin", Resource = "@layout/device_admin_sample")]
+	[IntentFilter(new[] {"android.app.action.DEVICE_ADMIN_ENABLED", Intent.ActionMain})]
+	public class AdminReceiver : DeviceAdminReceiver
+	{
+	}
+}
